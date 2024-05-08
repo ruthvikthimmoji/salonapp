4f632bcf3921ea6db7c6d4b57b1ac3801d68fc11
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2024-05-08 13:15:07 +0530

    signin page updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>
-        
-    </div>
+    // <div>
+          
+    //     <a href='/signin'> Signin</a>/ <a href='/login'> Login</a>
+    // </div>
+    
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <h1> Salon App </h1>
+  <figure><img src="salon1.png" alt="salon" /></figure>
+  <div className="card-body"> 
+      <a href='/login'> 
+       <button> 
+        Login
+       </button>
+       </a>
+       or
+       <a href='/signin'> 
+       <button> 
+        Signin
+       </button>
+       </a>
+  </div>
+</div>
+
+
   )
 }
 
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,11 +3,36 @@ import React from 'react'
 const sigIn = () => {
   return (
     <div>
-        <h1>SIGN IN PAGE</h1>
+    <h1>Salon App</h1>
+      <h1>SIGN-IN</h1>
+      <div className='container'>
+      <form action="/submit" method="post">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="email"/>
+          <label htmlFor="Phone Number">PhoneNumber</label>
+          <input type='tel' id='phone' name='Phone Number'></input>
+          <label htmlFor="password">Password</label>
+          <input type='password' id='password' name='password' placeholder='password'/>
+          
+          <input type="checkbox" id="checkbox" name="checkbox"/>
+          <button type="submit">Submit</button>
+          or 
+          signin with
+          <a href='#' className='google'>
+            <button type='./google.com'> google</button>
+            </a>
 
 
-    </div>
-  )
+      </form>
+
+        
+      </div>
+
+
+
+
+  </div>
+)
 }
 
 export default sigIn
