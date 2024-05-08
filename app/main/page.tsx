499974b152981396ab7ad7f68578808cf4aa11a4
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2024-05-08 10:22:46 +0530

    first commit added routes

---
+++ app/main/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './page.module.css'
+
+const main = () => {
+  return (
+    <div className={styles.container}>
+        main</div>
+  )
+}
+
+export default main
---
+++ app/signin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sigIn = () => {
+  return (
+    <div>sigIn</div>
+  )
+}
+
+export default sigIn
