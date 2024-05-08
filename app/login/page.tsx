e9b7a5e0474dbfede436eecf1b0894b6e12552dc
Ruthviikthimmoji <149005776+ruthvikthimmoji@users.noreply.github.com>
2024-05-09 00:23:30 +0530

    signup page updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,23 +10,17 @@ const LoginPage = () => {
         <h1>LOGIN PAGE</h1>
         <div className='container'>
         <form action="/submit" method="post">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="email"/>
-            {/* <label htmlFor="Phone Number">PhoneNumber</label> */}
-            {/* <input type='tel' id='phone' name='Phone Number'></input> */}
+            {/* <label htmlFor="email">Email</label> */}
+            {/* <input type="email" id="email" name="email" placeholder="email"/> */}
+            <label htmlFor="Phone Number">PhoneNumber</label>
+            <input type='tel' id='phone' name='Phone Number'></input>
             <label htmlFor="password">Password </label>
             <input type='password' id='password' name='password' placeholder='password'/>
             <a href='/profile'>
               <button >Login</button></a>
             or New user? create a account <a href='/signin'> signin</a>
         </form>
-
-          
         </div>
-
-
-
-
     </div>
     </div>
   )
---
+++ app/signup/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useState } from 'react'
+
+const signUp = () => {
+
+  const [phone, setPhone] = useState('');
+  const handlePhoneChange = (event: any) => {
+    setPhone(event.target.value);
+  };
+
+  function createUser(event: any){
+    event.preventDefault();
+    // console.log(phone);
+    // redirect to profile page
+  }
+
+  return (
+    <div>
+    <h1>Salon App</h1>
+      <h1>SIGN-UP</h1>
+      <div className='container'>
+      <form onSubmit={createUser}>
+          {/* <label htmlFor="email">Email</label> */}
+          {/* <input type="email" id="email" name="email" placeholder="email"/> */}
+          <label htmlFor="Phone Number">PhoneNumber</label>
+          <input type='tel' id='phone' name='Phone Number' value={phone} onChange={handlePhoneChange}></input>
+          <label htmlFor="otp">OTP</label>
+          <input type="number" id='otp' name='otp' placeholder='otp'/>
+          {/* <input type="checkbox" id="checkbox" name="checkbox"/> */}
+          <button type="submit">Submit</button>
+      </form>
+      </div>
+  </div>
+)
+}
+
+export default signUp
